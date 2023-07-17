f8567b6050c6d05842df004fc5959a9583aa0103
Binod Raj Dhami <binodraazdhami@gmail.com>
2023-07-17 14:02:10 +0545

    business plannig section completed

--- includes/business-planning.php
+++ includes/business-planning.php
@@ -1,5 +1,12 @@
 <section class="business-planning">
     <div class="container">
-
+        <div class="section-title">
+            <h4>DIGITAL SOLUTION</h4>
+            <h2>
+                <span>Good Business Planning</span>
+                <span>Ensure Success.</span>
+            </h2>
+            <a href="<?php bloginfo('url'); ?>/request-quote">Request Quote <i class="fa-solid fa-arrow-right"></i></a>
+        </div>
     </div>
 </section>
--- page-home.php
+++ page-home.php
@@ -1,10 +1,16 @@
 <?php
 get_header();
-?>
 
-<?php include_once('includes/banner.php'); ?>
-<?php include_once('includes/services.php'); ?>
-<?php include_once('includes/welcome.php'); ?>
+// import banner
+include_once('includes/banner.php');
 
-<?php
+// import services
+include_once('includes/services.php');
+
+// import welcome
+include_once('includes/welcome.php');
+
+// import business-planning
+include_once('includes/business-planning.php');
+
 get_footer();
